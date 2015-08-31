48652542ad96ea271af6b86024e8719b30ff2ecf
softprops <d.tangren@gmail.com>
2015-08-31 00:40:16 -0400

    issue list filters, may want to make a nicer interface for this...

--- src/issues.rs
+++ src/issues.rs
@@ -1,7 +1,57 @@
 use std::io::Result;
-use self::super::Github;
+use self::super::{Github, SortDirection, State};
 use rep::{Issue, IssueReq, Label};
 use rustc_serialize::json;
+use std::fmt;
+use std::default::Default;
+
+pub enum Filter {
+  Assigned,
+  Created,
+  Mentioned,
+  Subscribed,
+  All
+}
+
+impl fmt::Display for Filter {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    write!(f, "{}", match *self {
+      Filter::Assigned => "assigned",
+      Filter::Created => "created",
+      Filter::Mentioned => "mentioned",
+      Filter::Subscribed => "subscribed",
+      Filter::All => "all"
+    })
+  }
+}
+
+impl Default for Filter {
+  fn default() -> Filter {
+    Filter::Assigned
+  }
+}
+
+pub enum Sort {
+  Created,
+  Updated,
+  Comments
+}
+
+impl fmt::Display for Sort {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    write!(f, "{}", match *self {
+      Sort::Created => "created",
+      Sort::Updated => "updated",
+      Sort::Comments => "comments"
+    })
+  }
+}
+
+impl Default for Sort {
+  fn default() -> Sort {
+    Sort::Created
+  }
+}
 
 pub struct IssueRef<'a> {
   github: &'a Github<'a>,
@@ -55,7 +105,7 @@ impl<'a> Issues<'a> {
   }
 
   fn path(&self, more: &str) -> String {
-    format!("/repos/{}/{}/issues/{}", self.owner, self.repo, more)
+    format!("/repos/{}/{}/issues{}", self.owner, self.repo, more)
   }
 
   pub fn create(&self, is: &IssueReq) -> Result<Issue> {
@@ -67,5 +117,37 @@ impl<'a> Issues<'a> {
       )
     );
     Ok(json::decode::<Issue>(&body).unwrap())
+  }
+
+  pub fn list(
+    &self,
+    state: State,
+    sort: Sort,
+    direction: SortDirection,
+    assignee: Option<&'static str>,
+    creator: Option<&'static str>,
+    mentioned: Option<&'static str>,
+    labels: Vec<&'static str>,
+    since: Option<String>
+   ) -> Result<Vec<Issue>> {
+    let mut params = Vec::new();
+    params.push(format!("state={}", state));
+    params.push(format!("sort={}", sort));
+    params.push(format!("direction={}", direction));
+    if let Some(a) = assignee {
+      params.push(format!("assignee={}", a));
+    }
+    if let Some(c) = creator {
+      params.push(format!("creator={}", c));
+    }
+    let url = self.path(
+      &format!("?{}", params.join("&"))
+    );
+    let body = try!(
+      self.github.get(
+        &url
+      )
+    );
+    Ok(json::decode::<Vec<Issue>>(&body).unwrap())
   }
 }
