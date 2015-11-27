9f103bde5b36135002d93bafb34d3e73c03b98a6
softprops <d.tangren@gmail.com>
2015-11-27 11:29:04 +0900

    examples should not hault travis ci success

--- examples/gists.rs
+++ examples/gists.rs
@@ -4,20 +4,26 @@ extern crate hubcaps;
 use hyper::Client;
 use hubcaps::{GistReq, Github};
 use std::collections::HashMap;
+use std::env;
 
 fn main() {
-    let client = Client::new();
-    let github = Github::new(
-        format!("hubcaps/{}", env!("CARGO_PKG_VERSION")),
-        &client,
-        Some(env!("GITHUB_TOKEN"))
-    );
-    let mut files = HashMap::new();
-    files.insert("thename", "the contents");
-    let gist = github.gists().create(
-        &GistReq::new(
-            None, false, files
-        )
-    ).unwrap();
-    println!("{:?}", gist)
+    match env::var("GITHUB_TOKEN").ok() {
+        Some(token) => {
+            let client = Client::new();
+            let github = Github::new(
+                format!("hubcaps/{}", env!("CARGO_PKG_VERSION")),
+                &client,
+                Some(token)
+            );
+            let mut files = HashMap::new();
+            files.insert("thename", "the contents");
+            let gist = github.gists().create(
+                &GistReq::new(
+                    None, false, files
+                        )
+                    ).unwrap();
+            println!("{:?}", gist)
+        },
+        _ => println!("example missing GITHUB_TOKEN")
+    }
 }
--- examples/repos.rs
+++ examples/repos.rs
@@ -3,15 +3,21 @@ extern crate hubcaps;
 
 use hyper::Client;
 use hubcaps::Github;
+use std::env;
 
 fn main() {
-    let client = Client::new();
-    let github = Github::new(
-        format!("hubcaps/{}", env!("CARGO_PKG_VERSION")),
-        &client,
-        Some(env!("GITHUB_TOKEN"))
-    );
-    for repo in github.repos().list().unwrap() {
-        println!("{:?}", repo)
+    match env::var("GITHUB_TOKEN").ok() {
+        Some(token) => {
+            let client = Client::new();
+            let github = Github::new(
+                format!("hubcaps/{}", env!("CARGO_PKG_VERSION")),
+                &client,
+                Some(token)
+            );
+            for repo in github.repos().list().unwrap() {
+                println!("{:?}", repo)
+            }
+        },
+        _ => println!("example missing GITHUB_TOKEN")
     }
 }
