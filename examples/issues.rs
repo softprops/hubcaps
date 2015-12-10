bc72f8ab77f264e6ef3037121372a1f6f65e2a2e
softprops <d.tangren@gmail.com>
2015-12-10 13:01:56 +0900

    add issues example

---
+++ examples/issues.rs
@@ -0,0 +1,23 @@
+extern crate hyper;
+extern crate hubcaps;
+
+use hyper::Client;
+use hubcaps::{Github, IssueListReq};
+use std::env;
+
+fn main() {
+    match env::var("GITHUB_TOKEN").ok() {
+        Some(token) => {
+            let client = Client::new();
+            let github = Github::new(format!("hubcaps/{}", env!("CARGO_PKG_VERSION")),
+            &client,
+                                     Some(token));
+            let repo = github.repo("softprops", "hubcaps");
+            let issues = repo.issues();
+            for issue in issues.list(&IssueListReq::builder().build()).unwrap() {
+                println!("#{} {}", issue.number, issue.title);
+            }
+        },
+        _ => println!("example missing GITHUB_TOKEN")
+    }
+}
