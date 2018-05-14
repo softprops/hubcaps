629ec1306082099e90930f83d138b75144315dc2
softprops <d.tangren@gmail.com>
2018-05-13 20:18:19 -0400

    create an example for pull request files

---
+++ examples/pull_files.rs
@@ -0,0 +1,30 @@
+extern crate env_logger;
+extern crate futures;
+extern crate hubcaps;
+extern crate tokio_core;
+
+use std::env;
+
+use futures::Stream;
+use tokio_core::reactor::Core;
+
+use hubcaps::{Credentials, Github, Result};
+
+fn main() -> Result<()> {
+  drop(env_logger::init());
+  match env::var("GITHUB_TOKEN").ok() {
+    Some(token) => {
+      let mut core = Core::new()?;
+      let github = Github::new(
+        concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION")),
+        Credentials::Token(token),
+        &core.handle(),
+      );
+      for diff in core.run(github.repo("rust-lang", "rust").pulls().get(49536).files())? {
+        println!("{:#?}", diff);
+      }
+      Ok(())
+    }
+    _ => Err("example missing GITHUB_TOKEN".into()),
+  }
+}
