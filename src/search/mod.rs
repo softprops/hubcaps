4966e477fd24d7f3ac27330028b01e526364a1ac
Dale Wijnand <dale.wijnand@gmail.com>
2018-07-18 00:58:37 +0100

    Resolves a single_char_pattern lint warning
        warning: single-character string constant used as pattern
           --> src/search/mod.rs:233:44
            |
        233 |         let mut path = parsed.path().split("/").collect::<Vec<_>>();
            |                        --------------------^^^- help: try using a char instead: `parsed.path().split('/')`
            |
            = note: #[warn(single_char_pattern)] on by default
            = help: for further information visit https://rust-lang-nursery.github.io/rust-clippy/v0.0.212/index.html#single_char_pattern

--- src/search/mod.rs
+++ src/search/mod.rs
@@ -230,7 +230,7 @@ impl IssuesItem {
     pub fn repo_tuple(&self) -> (String, String) {
         // split the last two elements off the repo url path
         let parsed = url::Url::parse(&self.repository_url).unwrap();
-        let mut path = parsed.path().split("/").collect::<Vec<_>>();
+        let mut path = parsed.path().split('/').collect::<Vec<_>>();
         path.reverse();
         (path[1].to_owned(), path[0].to_owned())
     }
