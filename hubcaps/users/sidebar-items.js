53d4f5d1dfe30fa115441ed445951957db7d2a91
softprops <softprops@users.noreply.github.com>
2020-10-20 04:53:55 +0000

    deploy: b216117da1c7a61a9cbcb9b94ed3d0c53105d964

--- hubcaps/users/sidebar-items.js
+++ hubcaps/users/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["AuthenticatedUser","Information about current authenticated user"],["Contributors","reference to contributors associated with a github repo"],["User","User information"],["Users","Query user information"]]});
+initSidebarItems({"struct":[["AuthenticatedUser","Information about current authenticated user"],["Contributors","reference to contributors associated with a github repo"],["User","User information"],["UserEmail",""],["Users","Query user information"]]});
