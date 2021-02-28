cca78f46804bc1405a95d6c2361b65e8b1a19517
softprops <softprops@users.noreply.github.com>
2021-02-28 13:17:38 +0000

    deploy: a9e6616ecc7b8eef0ea0eaf6055d9610c8961500

--- implementors/core/convert/trait.Into.js
+++ implementors/core/convert/trait.Into.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["hubcaps"] = [{"text":"impl Into&lt;Vec&lt;u8&gt;&gt; for DecodedContents","synthetic":false,"types":[]}];
+implementors["hubcaps"] = [{"text":"impl Into&lt;Vec&lt;u8, Global&gt;&gt; for DecodedContents","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
